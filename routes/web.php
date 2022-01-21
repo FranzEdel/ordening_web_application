e161bba7a2c7fbf7248baf86b12934fcb2e09433
franzedel <franz.t77@gmail.com>
2022-01-21 04:11:00 -0400

    Design a pizza form

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PizzaController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/pizza', [PizzaController::class, 'index'])->name('pizza.index');
+
