a5daedb135ff7f49dc7f81e0ccfe63fe73f4452f
franzedel <franz.t77@gmail.com>
2022-01-27 01:59:32 -0400

    Validate store pizza

--- app/Http/Controllers/PizzaController.php
+++ app/Http/Controllers/PizzaController.php
@@ -3,11 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\PizzaStoreRequest;
 
 class PizzaController extends Controller
 {
     public function index()
     {
-        return view('pizza.index');
+        return 'List of pizza';
+    }
+
+    public function create()
+    {
+        return view('pizza.create');
+    }
+
+    public function store(PizzaStoreRequest $request)
+    {
+        //dd($request->all());
+
     }
 }
